b57ae8c38d0c682085545cb13cf0f15855adc9e5
rodolfopaesfarias <rodolfofarias@gmail.com>
2019-05-31 13:02:40 -0300

    31052019

--- src/app/home/sign-in/sign-in.component.ts
+++ src/app/home/sign-in/sign-in.component.ts
@@ -1,19 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './sign-in.component.html'
 })
 export class SignInComponent implements OnInit {
-    
+
     loginForm: FormGroup;
-    
-    constructor(private formBuilder: FormBuilder) { }
-    
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private authSvc: AuthService
+    ) { }
+
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
-            userName: ['', Validators.required],
+            username: ['', Validators.required],
             password: ['', Validators.required]
         });
+    }
+
+    login() {
+
+        const username = this.loginForm.get('username').value;
+        const password = this.loginForm.get('password').value;
+
+        console.log(username);
+        console.log(password);
+
+        this.authSvc.authenticate(username, password).subscribe(
+            () => console.log("autenticou"),
+            (error) => {
+                console.log(error);
+                this.loginForm.reset();
+                alert("erro ao autenticar.")
+            }
+        )
+
+
     }
 }
